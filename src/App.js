ebcbbce155acbb8dba21f2d260181802f6c7e8fa
LysenkoOleg <89065167+LysenkoOleg@users.noreply.github.com>
2024-10-27 13:00:09 +0300

    add prettier

--- src/App.js
+++ src/App.js
@@ -2,25 +2,33 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
+const sneakersData = [
+	{ name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: '12 999 руб.', image: '/img/sneakers/1.jpg' },
+	{ name: 'Мужские Кроссовки Nike Air Max 270', price: '12 999 руб.', image: '/img/sneakers/2.jpg' },
+	{ name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: '8 499 руб.', image: '/img/sneakers/3.jpg' },
+];
+
 function App() {
-  return (
-    <div className="wrapper clear">
+	return (
+		<div className='wrapper clear'>
 			<Drawer />
 			<Header />
 			<div className='content p-40'>
-				<div className="d-flex align-center justify-between mb-40">
+				<div className='d-flex align-center justify-between mb-40'>
 					<h1>Все кроссовки</h1>
-					<div className="search-block d-flex">
+					<div className='search-block d-flex'>
 						<img src='/img/search.svg' alt='search' />
-						<input type='text' placeholder="Поиск..." />
+						<input type='text' placeholder='Поиск...' />
 					</div>
 				</div>
 				<div className='d-flex'>
-					<Card />
+					{
+						sneakersData.map((data) => (<Card {...data} />))
+					}
 				</div>
 			</div>
 		</div>
-  );
+	);
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({name, price, image}) => {
 	return (
 		<div className='card'>
 			<div className='favourite'>
 				<img src='/img/heart-unliked.svg' alt='unliked' />
 			</div>
-			<img className='sneaker' width={133} height={112} src='/img/sneakers/1.jpg' alt='' />
-			<h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+			<img className='sneaker' width={133} height={112} src={image} alt='' />
+			<h5>{name}</h5>
 			<div className="d-flex justify-between align-center">
 				<div className="d-flex flex-column">
 					<span>Цена: </span>
-					<b>12 999 руб.</b>
+					<b>{price}</b>
 				</div>
 				<button className="button">
 					<img src='/img/btn-plus.svg' alt='btn-plus' />
